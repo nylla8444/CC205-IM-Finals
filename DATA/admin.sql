73b046b1b1576664886eba194e4167a21f1ebcf8
Allyn <110903853+nylla8444@users.noreply.github.com>
2025-05-22 16:53:05 +0800

    fix: removed _v admin.sql

--- DATA/admin.sql
+++ DATA/admin.sql
@@ -1,4 +1,4 @@
-INSERT INTO ADMIN (_id, admin_id, password, adminCode, createdAt, updatedAt, __v) VALUES (
+INSERT INTO ADMIN (_id, admin_id, password, adminCode, createdAt, updatedAt) VALUES (
   1,
   123,
   '$2b$10$p.ekhHz1w555OpQ3x2uH..noCAOqZvJu0X81u2tIMITdvdJUym1su',
